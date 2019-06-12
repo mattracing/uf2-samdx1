303684fa4e61835f787da28fe85fe67e83ec2803
mattracing <39485955+mattracing@users.noreply.github.com>
2019-06-12 12:07:30 -0300

    Add files via upload

---
+++ boards/nonthue_m0/board_config.h
@@ -0,0 +1,19 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "Eltech"
+#define PRODUCT_NAME "Nonthue M0"
+#define VOLUME_LABEL "NONTHUEBOOT"
+#define INDEX_URL "http://www.eltech.com.ar/Nonthue"
+#define BOARD_ID "SAMD21G18A-Nonthue-M0-v0"
+
+#define USB_VID 0x239A
+#define USB_PID 0x001B
+
+#define LED_PIN PIN_PA17
+
+#define BOARD_NEOPIXEL_PIN PIN_PA06
+#define BOARD_NEOPIXEL_COUNT 1
+
+
+#endif
